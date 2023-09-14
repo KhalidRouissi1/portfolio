6fb039e1d32fff3fc837385dfba92ed9dbf331e9
KhalidRouissi1 <khalidrouissi7@gmail.com>
2023-09-14 22:01:54 +0100

    Responsive things & some edits

--- Script/script.js
+++ Script/script.js
@@ -10,9 +10,33 @@ window.onscroll = function(){
         document.querySelector(".aboutMe").classList.add("addAnim")
     }
 
+    if(scrollY > 150 ){
+        dropDownMenu.style.display = "none";
+    }
  
  
+    
 }
+window.onresize = function() {
+    if (window.innerWidth > 768) {
+        dropDownMenu.style.display = "none";
+
+
+
+    }
+    if (window.innerWidth <768) {
+        dropDownMenu.style.display = "none";
+        let op  =  document.querySelector(".otherProjects");
+        op.id = "Work"
+    }
+
+
+
+    
+
+};
+let dropDownMenu =   document.querySelector(".dropDownMenu");
+
 
 let work = document.querySelector(".work");
 
@@ -20,3 +44,15 @@ let work = document.querySelector(".work");
 work.addEventListener("clcik",()=>{
     work.style.cssText = ``;
 });
+
+let menuBtn = document.querySelector(".menuBtn");
+var toggle = false;
+
+menuBtn.addEventListener("click",()=>
+{   
+    console.log(toggle)
+    toggle = !toggle;
+    toggle ? dropDownMenu.style.display ="block" :dropDownMenu.style.display ="none" ;
+
+})
+
